37a4f6531b8538bbbbdd7d50008fe963cfd8d8ec
Fatah ZAMICHE <zamiche.fatah@gmail.com>
2025-01-09 15:32:57 +0100

    Add Routing 1. `home`using `routerLink` 2. `about` using injection of `Router`

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ContactFormComponent} from "./contact-form/contact-form.component";
+import {AboutComponent} from "./about/about.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home",
+    component: ContactFormComponent
+  },
+  {
+    path: "about",
+    component: AboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
