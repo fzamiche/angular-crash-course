1967a9810cd1504cdf303bd888e67fbb3a94fc6c
Fatah ZAMICHE <zamiche.fatah@gmail.com>
2025-01-09 15:51:01 +0100

    Impl how to get `pathVariable` with `ActivatedRoute`

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+
+  constructor(private activatedRoute: ActivatedRoute) {}
+
+
+  pathVariable: any;
+
+  ngOnInit(): void {
+    console.log(this.activatedRoute);
+    this.pathVariable = this.activatedRoute.snapshot.params['pathVariable'];
+  }
 
 }
